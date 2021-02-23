b43193e4ed8a0b6d8d1c3322063b1bd6e05e41d4
Jon Ganebski <jon.ganebski@gmail.com>
2021-02-23 16:25:31 +0900

    basepath fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports =
   process.env.NODE_ENV === "production"
     ? {
-        basePath: "/jonganebski-test.github.io",
+        basePath: "",
         assetPrefix: "/jonganebski-test.github.io",
       }
     : {
